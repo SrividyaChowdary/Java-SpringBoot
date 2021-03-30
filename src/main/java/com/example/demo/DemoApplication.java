68c93fd7dcb796af0172d75dab072ec9d2798e5d
SrividyaChowdary <32638060+SrividyaChowdary@users.noreply.github.com>
2021-03-30 21:07:37 +0530

    spring and tomcat check

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.example"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
